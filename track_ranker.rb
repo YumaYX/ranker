19e804d955ba3ff85b01cda423d1784426596418
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-11-15 01:21:52 +0900

    first commit

---
+++ track_ranker.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'json'
+require_relative 'music_track'
+
+def itunes_most_played
+  url = 'https://rss.applemarketingtools.com/api/v2/jp/music/most-played/100/songs.json'
+  # url = './songs.json'
+  content = URI.open(url).read
+  JSON.parse(content)['feed']['results']
+end
+
+def itunes_ranking
+  tracks = []
+  index = 1
+  itunes_most_played.each do |ele|
+    track = MusicTrack.new(ele['name'], ele['artistName'])
+
+    next if tracks.any? { |i| i.info == track.info }
+
+    track.ranking = index
+    tracks << track
+    index += 1
+  end
+  tracks
+end
+
+def md_table(*arr)
+  "| #{arr.join(' | ')} |\n"
+end
+
+if __FILE__ == $PROGRAM_NAME
+  content = md_table(%w[Rank Songs])
+  content << md_table(['---', '---'])
+  itunes_ranking.each do |track|
+    content << md_table([track.ranking, track.info])
+  end
+
+  Dir.mkdir('_site') unless Dir.exist?('_site')
+  File.write('_site/index.md', content)
+end
